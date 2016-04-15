bf61f5a09a1581f7e8059a249a7ea6ffc1208697
Max Melnick <maxmelnick@Maxs-MacBook-Pro.local>
2016-04-15 16:49:07 -0400

    updated script to automatically start jupyter notebook on vagrant up or vagrant reload

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
-apt-get update
+start_seconds="$(date +%s)"
+echo "Welcome to the initialization script."
 
-#install git
-apt-get install -y git
+ping_result="$(ping -c 2 8.8.4.4 2>&1)"
+if [[ $ping_result != *bytes?from* ]]; then
+    echo "Network connection unavailable. Try again later."
+    exit 1
+fi
+
+# install git
+apt_packages=(
+    git
+)
+
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Installing apt-get packages..."
+sudo apt-get install -y ${apt_packages[@]}
+sudo apt-get clean
 
 #install anaconda
-miniconda=Miniconda2-latest-Linux-x86_64.sh
+anaconda=Anaconda2-4.0.0-Linux-x86_64.sh
 cd /vagrant
-if [[ ! -f $miniconda
-    wget --quiet http://repo.continuum.io/miniconda/$miniconda
+if [ ! -f $anaconda ]
+	then
+		echo "No Anaconda install file pre-loaded. Downloading Anaconda"
+		echo "NOTE: downloading and installing Anaconda2-4.0.0-Linux-x86_64.sh"
+    	wget --quiet http://repo.continuum.io/archive/$anaconda
 fi
-chmod +x $miniconda
-./$miniconda -b -p /opt/anaconda
+chmod +x $anaconda
+
+sudo -u root ./$anaconda -b -p /opt/anaconda
 
 conda_path=/opt/anaconda/bin
 echo $conda_path
@@ -22,44 +42,48 @@ cat >> /home/vagrant/.bashrc << END
 PATH=:$conda_path:\$PATH
 END
 
-#install jupyter
-$conda_path/conda install jupyter
+# Preemptively accept Github's SSH fingerprint, but only
+# if we previously haven't done so.
+fingerprint="$(ssh-keyscan -H github.com)"
+if ! grep -qs "$fingerprint" ~/.ssh/known_hosts; then
+    echo "$fingerprint" >> ~/.ssh/known_hosts
+fi
 
-#install numpy
-$conda_path/conda install numpy
+# Vagrant should've created /srv/www according to the Vagrantfile,
+# but let's make sure it exists even if run directly.
+if [[ ! -d '/srv/notebooks' ]]; then
+    sudo mkdir '/srv/notebooks'
+    sudo chown vagrant:vagrant '/srv/notebooks'
+fi
 
-#install pandas
-$conda_path/conda install pandas
 
-#install scipy
-$conda_path/conda install scipy
+jupyter=$conda_path/jupyter-notebook
+log="/home/vagrant/jupyter.log"
+jupyter_args="--ip=0.0.0.0 --notebook-dir=/srv/notebooks"
+run="start-stop-daemon --start --chuid vagrant:vagrant --exec $jupyter -- $jupyter_args >> $log 2>&1 &"
+echo $run
+eval $run
 
-#install matplotlib
-$conda_path/conda install matplotlib
+cat << UPSTART | sudo tee /etc/init/jupyter.conf > /dev/null
+description "Anaconda for Data Science"
+author "maxmelnick <maxmelnick@gmail.com>"
 
-#install statsmodels
-$conda_path/conda install statsmodels
+start on vagrant-mounted MOUNTPOINT=/srv/notebooks
 
-#install seaborn
-$conda_path/conda install seaborn
+exec $run
+UPSTART
 
-#install dependencies for reading from html
-$conda_path/conda install beautiful-soup
-$conda_path/conda install html5lib
-$conda_path/conda install lxml
+end_seconds="$(date +%s)"
+echo "-----------------------------"
+echo "Provisioning complete in "$(expr $end_seconds - $start_seconds)" seconds"
+echo "You can now use 'less -S +F $log' to monitor jupyter notebook server."
 
-#install dependencies for reading data from Excel
-$conda_path/conda install xlrd
-$conda_path/conda install openpyxl
 
-#install scikit-learn
-$conda_path/conda install scikit-learn
 
-#install statsmodels
-$conda_path/conda install statsmodels
 
-#install spyder
-$conda_path/conda install spyder
 
-#install numexpr (needed for .query() on pandas dataframe)
-$conda_path/conda install numexpr
+
+
+
+
+
